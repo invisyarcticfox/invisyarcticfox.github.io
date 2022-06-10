85245b5b3fafde8de2ceec92824b184fa0d2fca0
Lucas <68004605+shamshitty@users.noreply.github.com>
2022-06-10 21:03:16 +0100

    did some shit

---
+++ assets/typewriter.js
@@ -0,0 +1,10 @@
+var messageArray = ["Lucas."];
+var textPosition = 0;
+var speed = 100;
+
+    typewriter = () => {
+        document.querySelector("#message").innerHTML = messageArray[0].substring(0, textPosition) + "<span>|</span>";
+        if(textPosition++ != messageArray[0].length)setTimeout(typewriter, speed);
+    }
+
+window.addEventListener("load", typewriter);
