7bf5c14499c2d5e1f0fefb7502f48c8f5306d317
Lucas <68004605+shamshitty@users.noreply.github.com>
2022-06-12 14:37:25 +0100

    more shit

---
+++ assets/master.js
@@ -0,0 +1,22 @@
+var messageArray = ["Lucas."];
+var textPosition = 0;
+var speed = 100;
+
+    typewriter = () => {
+        document.querySelector("#name").innerHTML = messageArray[0].substring(0, textPosition) + "<span>|</span>";
+        if(textPosition++ != messageArray[0].length)setTimeout(typewriter, speed);
+    }
+
+window.addEventListener("load", typewriter);
+
+//
+
+function copy() {
+    var copyText = document.getElementById("copy").innerText;
+    var elem = document.createElement("textarea");
+    document.body.appendChild(elem);
+    elem.value = copyText;
+    elem.select();
+    document.execCommand("copy");
+    document.body.removeChild(elem);
+}
