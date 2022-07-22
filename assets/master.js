d6069077c23bc07e54d53568a3fe54880cae8e5a
Lucas <68004605+shamshitty@users.noreply.github.com>
2022-07-22 12:12:07 +0100

    ugh

--- assets/master.js
+++ assets/master.js
@@ -3,7 +3,7 @@ var textPosition = 0;
 var speed = 100;
 
     typewriter = () => {
-        document.querySelector("#name").innerHTML = messageArray[0].substring(0, textPosition) + "<span>|</span>";
+        document.querySelector("#name").innerHTML = messageArray[0].substring(0, textPosition) + '<span class="blinker">|</span>';
         if(textPosition++ != messageArray[0].length)setTimeout(typewriter, speed);
     }
 
