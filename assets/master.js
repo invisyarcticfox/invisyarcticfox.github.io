b2f23a1ee90c53b002192f9ad6bcd3c856b207e0
Lucas <iaminvisy@gmail.com>
2023-09-01 01:16:10 +0100

    removed light mode
    aiohpaioshg i didnt like it

--- assets/master.js
+++ assets/master.js
@@ -1,34 +1,3 @@
-// LIGHTMODE TOGGLE //
-// https://www.youtube.com/watch?v=wodWDIdV9BY
-
-let LightMode = localStorage.getItem("lightmode");
-const lightModeToggle = document.querySelector("#lm-toggle");
-
-const enableLightMode = () => {
-  document.body.classList.add("lightmode");
-  localStorage.setItem("lightmode", "true");
-};
-const disableLightMode = () => {
-  document.body.classList.remove("lightmode");
-  localStorage.setItem("lightmode", "false");
-};
-
-if (LightMode === "true") {
-  enableLightMode();
-}
-
-lightModeToggle.addEventListener("click", () => {
-  LightMode = localStorage.getItem("lightmode");
-  if (LightMode !== "true") {
-    enableLightMode();
-    console.log(true);
-  } else {
-    disableLightMode();
-    console.log(false)
-  }
-})
-
-
 // URL PARAMETER //
 // Parse the URL parameter
 function getParameterByName(name, url) {
