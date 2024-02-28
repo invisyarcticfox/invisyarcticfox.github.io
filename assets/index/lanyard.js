1d60f95d710c5e01fca9de2cd7f9d22321892faf
Lucas <iaminvisy@gmail.com>
2024-02-28 23:07:44 +0000

    switched to lanyard socket
    - https://github.com/Phineas/lanyard?tab=readme-ov-file#socket-docs
    - fixed 404
    -added pridecorner even tho i had it before??

--- assets/index/lanyard.js
+++ assets/index/lanyard.js
@@ -1,67 +1,61 @@
-const APIRUL = 'https://api.lanyard.rest/v1/users/'
-const DISCCDN = 'https://cdn.discordapp.com'
-const USERID = '470193291053498369'
-const pfp = document.getElementById('pfp')
-const spotifylink = document.querySelector('.spotifylink')
-const spotifytt = document.querySelector('.tooltip .txt')
+const ws = new WebSocket('wss://api.lanyard.rest/socket');
+const discordurl = 'https://cdn.discordapp.com';
+const userid = '470193291053498369';
+const avatar = document.querySelector('#avatar #pfp');
+const spotifylink = document.querySelector('.spotifylink');
+const spotifytt = document.querySelector('.tooltip');
+const spotifytxt = document.querySelector('.tooltip .txt');
 
-async function fetchResponse(USERID) {
+
+ws.onopen = console.log('WebSocket open!')
+ws.onmessage = ({data: msg}) => {
   try {
-    const res = await fetch(APIRUL+USERID)
-    return await res.json()
-  } catch (err) {
-    console.error(err)
-  }
-}
-
-async function setAvatar() {
-  const {
-    data: {
-      discord_user: {
-        avatar
-      }
+    const data = JSON.parse(msg);
+    console.log(data)
+    switch (data.op) {
+      case 1:
+        ws.send(JSON.stringify({
+          op: 2,
+          d: {
+            subscribe_to_id: userid
+          }
+        }));
+        setInterval(() => {
+          ws.send(JSON.stringify({
+            op: 3
+          }))
+        }, data.d.heartbeat_interval);
+        break
     }
-  } = await fetchResponse(USERID)
-  const avaturl = `${DISCCDN}/avatars/${USERID}/${avatar}?size=512`
-  pfp.src = avaturl
-}
 
-async function setListening() {
-  const {
-    data: {
-      listening_to_spotify
-    }
-  } = await fetchResponse(USERID)
-  if(!listening_to_spotify) {
-    spotifylink.removeAttribute('href')
-    spotifylink.removeAttribute('style')
-    return
-  } else {
-    const {
-      data: {
-        spotify: {
-          track_id,
-          artist,
-          song
-        }
+
+    // idle: #f0b232
+    // online: #23a55a
+    // dnd: #f23f43
+    // offline: #80848e
+
+
+    avatar.src = discordurl+'/avatars/'+userid+'/'+data.d.discord_user.avatar+'?size=512';
+    // i hate long urls >:(
+
+    if(data.d.listening_to_spotify === true){
+      const artistsplit = data.d.spotify.artist.split(';')
+
+      spotifylink.style.display = 'block';
+      spotifytt.style = ''
+      spotifytxt.innerHTML = data.d.spotify.song+' - '+artistsplit[0]
+
+      if(data.d.spotify.track_id !== null) {
+        spotifylink.href = 'https://open.spotify.com/track/'+data.d.spotify.track_id
+      } else {
+        spotifylink.removeAttribute('href')
       }
-    } = await fetchResponse(USERID)
-    spotifylink.style.display = 'block'
-    spotifytt.innerHTML = `${song} - ${artist}`
-    spotifylink.style.cursor = ''
-    spotifylink.href = `https://open.spotify.com/track/${track_id}`
-    if(!track_id) {
+    } else {
+      spotifylink.style.display = 'none';
+      spotifytt.style.display = 'none'
+      spotifytxt.innerHTML = ''
       spotifylink.removeAttribute('href')
-      spotifylink.style.cursor = 'default'
     }
-  }
-}
 
-function invoke() {
-  console.log(APIRUL+USERID)
-  setInterval(() => {
-    setListening()
-  }, 2500);
-  setAvatar()
+  } catch{}
 }
-invoke()
